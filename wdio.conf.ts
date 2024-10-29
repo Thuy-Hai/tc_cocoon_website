5c8dd99b839a7260f0167684108936287700c1fa
Hai0692 <ttthai.20it2@vku.udn.vn>
2024-10-29 16:07:55 +0700

    update config selenium

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -109,7 +109,10 @@ export const config: WebdriverIO.Config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [["visual",{drivers:{chrome:"130.0.6723.70 "}}]],
+  services: [
+    ["visual", { drivers: { chrome: "130.0.6723.70" } }],
+    ["selenium-standalone", { drivers: { chrome: true } }],
+  ],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
